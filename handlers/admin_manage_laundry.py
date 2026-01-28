042effa342ceb6e5887f0b7b281f4771d58f82ad
AntonLodygin <tonylodygin123@gmail.com>
2026-01-28 21:11:02 +0300

    exit from manage machines

--- handlers/admin_manage_laundry.py
+++ handlers/admin_manage_laundry.py
@@ -1,6 +1,6 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from keyboards.admin_manage_laundry_keyboards import get_machines_kb
 from config import ADMIN_CHAT_ID
@@ -12,3 +12,8 @@ manage_laundry_router = Router()
 async def manage_laundry(message: Message):
     if str(message.chat.id) == ADMIN_CHAT_ID:
         await message.answer("Выберите машину:", reply_markup=get_machines_kb())
+
+
+@manage_laundry_router.callback_query(F.data == "exit_from_manage_machines")
+async def exit_from_manage_machines(cb: CallbackQuery):
+    await cb.message.delete()
--- keyboards/admin_manage_laundry_keyboards.py
+++ keyboards/admin_manage_laundry_keyboards.py
@@ -4,7 +4,7 @@ from database.db import get_machine_names
 
 
 def get_machines_kb() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(
-        inline_keyboard=[[InlineKeyboardButton(text=name, callback_data="empty")] for name in get_machine_names()]
-    )
+    btns = [[InlineKeyboardButton(text=name, callback_data="empty")] for name in get_machine_names()]
+    btns.append([InlineKeyboardButton(text="Отмена", callback_data="exit_from_manage_machines")])
+    return InlineKeyboardMarkup(inline_keyboard=btns)
 
